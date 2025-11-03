5def947b1db3606a04f36a4321d78c2c08da7281
ToanND <toannguyencnptit2k5@gmail.com>
2025-11-03 14:14:34 +0700

    fix bug

--- src/main/java/ptit/edu/vn/bookshop/domain/dto/request/CheckoutRequestDTO.java
+++ src/main/java/ptit/edu/vn/bookshop/domain/dto/request/CheckoutRequestDTO.java
@@ -1,10 +1,23 @@
 package ptit.edu.vn.bookshop.domain.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 public class CheckoutRequestDTO {
     private Long addressId;
+    private List<CheckoutCartItemsRequestDTO> cartItems;
     private String couponCode;
 
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CheckoutCartItemsRequestDTO {
+        private Long id;
+    }
 }
--- src/main/java/ptit/edu/vn/bookshop/service/impl/CouponServiceImpl.java
+++ src/main/java/ptit/edu/vn/bookshop/service/impl/CouponServiceImpl.java
@@ -1,17 +1,31 @@
 package ptit.edu.vn.bookshop.service.impl;
 
 ;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import ptit.edu.vn.bookshop.domain.constant.BookStatusEnum;
+import ptit.edu.vn.bookshop.domain.constant.StatusEnum;
 import ptit.edu.vn.bookshop.domain.dto.request.CouponCreateRequestDTO;
 import ptit.edu.vn.bookshop.domain.dto.request.CouponUpdateRequestDTO;
 import ptit.edu.vn.bookshop.domain.dto.response.CouponResponseDTO;
+import ptit.edu.vn.bookshop.domain.dto.response.page.CategoryPageResponseDTO;
+import ptit.edu.vn.bookshop.domain.dto.response.page.CouponPageResponseDTO;
+import ptit.edu.vn.bookshop.domain.entity.Category;
 import ptit.edu.vn.bookshop.domain.entity.Coupon;
 import ptit.edu.vn.bookshop.exception.IdInvalidException;
 import ptit.edu.vn.bookshop.domain.dto.mapper.CouponMapper;
 import ptit.edu.vn.bookshop.repository.CouponRepository;
+import ptit.edu.vn.bookshop.repository.specification.CategorySpecificationBuilder;
+import ptit.edu.vn.bookshop.repository.specification.CouponSpecificationBuilder;
 import ptit.edu.vn.bookshop.service.CouponService;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Service
 public class CouponServiceImpl implements CouponService {
@@ -92,6 +106,56 @@ public class CouponServiceImpl implements CouponService {
         this.couponMapper.updateCouponFromDto(couponRequestDTO, coupon);
         return this.couponMapper.toResponseDTO(this.couponRepository.save(coupon));
     }
+
+    @Override
+    public void deleteCoupon(Long id) {
+        Coupon coupon = this.couponRepository.findById(id)
+                .orElseThrow(() -> new IdInvalidException("Coupon is not found"));
+        coupon.setStatus(StatusEnum.DELETED);
+        this.couponRepository.save(coupon);
+    }
+
+    @Override
+    public CouponResponseDTO getCoupon(Long id, boolean isAdmin) {
+        Coupon coupon = this.couponRepository.findById(id)
+                .orElseThrow(() -> new IdInvalidException("Coupon is not found"));
+        if (!isAdmin && coupon.getStatus().equals(StatusEnum.DELETED)) {
+            throw new IllegalArgumentException("Coupon is already deleted");
+        }
+        return this.couponMapper.toResponseDTO(coupon);
+    }
+
+    @Override
+    public CouponPageResponseDTO getAllCoupons(Pageable pageable, String[] coupon, boolean isAdmin) {
+        CouponSpecificationBuilder builder = new CouponSpecificationBuilder();
+        if (coupon != null && coupon.length > 0) {
+            for (String p : coupon) {
+                Pattern pattern = Pattern.compile("(\\w+?)([:<>~!])(.*)(\\p{Punct}?)(.*)(\\p{Punct}?)");
+                Matcher matcher = pattern.matcher(p);
+                if (matcher.find()) {
+                    builder.with(
+                            matcher.group(1),
+                            matcher.group(2),
+                            matcher.group(3),
+                            matcher.group(4),
+                            matcher.group(5));
+                }
+            }
+        }
+        if (!isAdmin) {
+            builder.with("status", ":", StatusEnum.ACTIVE.name(), "", "");
+        }
+
+        Page<Coupon> couponPage = this.couponRepository.findAll(builder.build(), pageable);
+        CouponPageResponseDTO responseDTO = new CouponPageResponseDTO();
+        responseDTO.setPage(pageable.getPageNumber() + 1);
+        responseDTO.setPageSize(pageable.getPageSize());
+        responseDTO.setTotal(couponPage.getTotalElements());
+        responseDTO.setPages(couponPage.getTotalPages());
+        responseDTO.setCoupons(couponPage.stream().map(couponMapper::toResponseDTO).collect(Collectors.toList()));
+        return responseDTO;
+    }
+
 
     @Override
     public Coupon getCouponByCode(String code) {
