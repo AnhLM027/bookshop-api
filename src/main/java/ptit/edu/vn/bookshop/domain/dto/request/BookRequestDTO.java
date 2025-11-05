8b97f993e0b3de30c6bcc35fe94cb81d21f174ca
ToanND <toannguyencnptit2k5@gmail.com>
2025-11-05 14:28:30 +0700

    okok

--- src/main/java/ptit/edu/vn/bookshop/domain/dto/request/BookRequestDTO.java
+++ src/main/java/ptit/edu/vn/bookshop/domain/dto/request/BookRequestDTO.java
@@ -15,10 +15,6 @@ public class BookRequestDTO {
 
     @NotBlank(message = "Book name is required")
     @Size(min = 1, max = 255, message = "Book name must be between 1 and 255 characters")
-    @Pattern(
-            regexp = "^[A-Za-zÀ-ỹ0-9\\s&.,!?():;'\"-]+$",
-            message = "Book name contains invalid characters"
-    )
     private String name;
 
     @Size(max = 2000, message = "Description must not exceed 2000 characters")
